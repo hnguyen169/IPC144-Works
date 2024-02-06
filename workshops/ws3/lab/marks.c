aa9af94bece89aed29650d914023f7db391b73c0
Harrison Nguyen <hnguyen169@myseneca.ca>
2024-02-06 10:41:45 -0500

    prototyping complete

--- workshops/ws3/lab/marks.c
+++ workshops/ws3/lab/marks.c
@@ -1,2 +1,20 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+
+void prnGrade(char grade) {
+}
+
+int getNoOfStudents(void) {
+	int value = 0;
+	return value;
+}
+
+int getMark(int maximum) {
+	int percentage = 0;
+	return percentage;
+}
+
+char grade(int mark) {
+	char ret = 0;
+	return ret;
+}

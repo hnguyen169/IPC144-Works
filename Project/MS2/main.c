2c9929b1498263006828a845dafb058925bcca41
Harrison Nguyen <hnguyen169@myseneca.ca>
2024-04-02 11:26:21 -0400

    starting milestone 2

--- Project/MS2/main.c
+++ Project/MS2/main.c
@@ -1,3 +1,18 @@
+/* Citation and Sources...
+Final Project Milestone 2
+Module: main
+Filename: main.c
+Version 1.0
+Author	Harrison Nguyen
+Revision History
+-----------------------------------------------------------
+Date      Reason
+
+-----------------------------------------------------------
+I have done all the coding by myself and only copied the code
+that my professor provided to complete my project milestones.
+-----------------------------------------------------------*/
+
 // Final Project 
 // Student POS App tester program
 // Version 1.0

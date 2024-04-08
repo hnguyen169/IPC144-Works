97e4ac2470a0093f38d8362501aab3a7d903a2fe
Harrison Nguyen <hnguyen169@myseneca.ca>
2024-04-08 16:49:21 -0400

    starting ms32

---
+++ Project/MS3/MS32/POS.h
@@ -0,0 +1,25 @@
+/* Citation and Sources...
+Final Project Milestone 3
+Module: POS
+Filename: POS.h
+Version 1.0
+Author	Harrison Nguyen
+Revision History
+-----------------------------------------------------------
+Date      Reason
+
+-----------------------------------------------------------
+I have done all the coding by myself and only copied the code
+that my professor provided to complete my project milestones.
+-----------------------------------------------------------*/
+
+#ifndef SENECA_POS_H
+#define SENECA_POS_H
+
+#define TAX 0.13
+#define MAX_SKU_LEN 6
+#define MAX_STOCK_NUMBER 999
+#define MAX_NO_ITEMS 500
+#define MAX_BILL_ITEMS 10
+
+#endif // !SENECA_POS_H
---
+++ Project/MS3/MS32/PosApp.h
@@ -0,0 +1,40 @@
+/* Citation and Sources...
+Final Project Milestone 3
+Module: PosApp
+Filename: PosApp.h
+Version 1.0
+Author	Harrison Nguyen
+Revision History
+-----------------------------------------------------------
+Date      Reason
+
+-----------------------------------------------------------
+I have done all the coding by myself and only copied the code
+that my professor provided to complete my project milestones.
+-----------------------------------------------------------*/
+
+#ifndef SENECA_POSAPP_H
+#define SENECA_POSAPP_H
+#include "POS.h"
+
+struct Item {
+	char SKU[MAX_SKU_LEN + 1];
+	char name[61];
+	double price;
+	int taxed;
+	int qty;
+};
+
+double cost(const struct Item* item);
+int loadItems(const char filename[]);
+void saveItems(const char filename[]);
+void start(const char* action);
+void inventory(void);
+void addItem(void);
+void removeItem(void);
+void stockItem(void);
+void POS(void);
+void listItems(void);
+
+#endif // !SENECA_POSAPP_H
+
